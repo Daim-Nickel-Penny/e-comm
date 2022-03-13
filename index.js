bb311cd8ea58e7b1e25d41ab244dd5393d8070a7
1804449@kiit.ac.in <1804449@kiit.ac.in>
2022-03-13 13:29:34 +0530

    Querying done. Relating one to many done

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { productsData, categories, categoriesData } = require("./initial_data");
 
 const typeDefs = gql`
   type Query {
     hello: String
+    products: [Product!]!
+    getProduct(id: ID!): Product
+    categories: [Category!]!
+    getCategory(id: ID!): Category!
+  }
+
+  type Product {
+    name: String!
+    image: String!
+    description: String!
+    quantity: Int!
+    price: Float!
+    onSale: Boolean!
+  }
+
+  type Category {
+    id: ID!
+    name: String!
+    products: [Product!]!
   }
 `;
 const resolvers = {
   Query: {
     hello: () => {
       return "Hello World";
+    },
+    products: () => {
+      return productsData;
+    },
+    getProduct: (parent, args) => {
+      const product = productsData.find((product) => product.id === args.id);
+
+      if (!product) return null;
+
+      return product;
+    },
+    categories: () => {
+      return categories;
+    },
+    getCategory: (parent, args, context) => {
+      const category = categoriesData.find(
+        (category) => category.id === args.id
+      );
+      if (!category) return null;
+      return category;
+    },
+  },
+  Category: {
+    products: (parent, args, context) => {
+      const products = productsData.filter(
+        (product) => product.categoryId === parent.id
+      );
+      if (!products) return null;
+      return products;
     },
   },
 };
