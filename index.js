e355dda8ee207f45485ab0b2c9747cfcbf65f298
1804449@kiit.ac.in <1804449@kiit.ac.in>
2022-03-13 12:29:59 +0530

    Setup Done

---
+++ index.js
@@ -0,0 +1,23 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+const resolvers = {
+  Query: {
+    hello: () => {
+      return "Hello World";
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
