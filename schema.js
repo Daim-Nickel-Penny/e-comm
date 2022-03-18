9406567e6f3f31fc783eabb142ea7bf1b33ba208
1804449@kiit.ac.in <1804449@kiit.ac.in>
2022-03-18 11:40:35 +0530

    Update Mutation Done

--- schema.js
+++ schema.js
@@ -16,6 +16,9 @@ exports.typeDefs = gql`
     deleteCategory(id: ID!): Boolean!
     deleteProduct(id: ID!): Boolean!
     deleteReview(id: ID!): Boolean!
+    updateCategory(id: ID!, input: UpdateCategoryInput!): Category
+    updateProduct(id: ID!, input: UpdateProductInput!): Product
+    updateReview(id: ID!, input: UpdateReviewInput!): Review
   }
 
   type Product {
@@ -62,6 +65,28 @@ exports.typeDefs = gql`
   }
 
   input AddReviewInput {
+    date: String!
+    title: String!
+    comment: String!
+    rating: Int!
+    productId: ID!
+  }
+
+  input UpdateCategoryInput {
+    name: String!
+  }
+
+  input UpdateProductInput {
+    name: String!
+    description: String!
+    quantity: Int!
+    image: String!
+    price: Float!
+    onSale: Boolean!
+    categoryId: String
+  }
+
+  input UpdateReviewInput {
     date: String!
     title: String!
     comment: String!
